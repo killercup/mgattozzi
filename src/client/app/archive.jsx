ea60d7b0fdca0fa14a5e7cbada48f7b0b3846e8c
Michael Gattozzi <mgattozzi@gmail.com>
2016-12-26 23:08:07 -0500

    checkpoint

--- src/client/app/archive.jsx
+++ src/client/app/archive.jsx
@@ -4,7 +4,93 @@ class Archive extends React.Component {
 
   render () {
     return(
-        <div>ARCHIVE</div>
+      <div>
+        <h1>Archive</h1>
+          <p>
+            You'll find collections of articles I've written here that can be
+            grouped together by topic
+          </p>
+        <h2>Alchemist</h2>
+          <p>
+            Articles regarding my package name conversion tool.
+          </p>
+          <ul>
+            <li><a href="/posts/announcing-alchemist.html">
+              Announcing Alchemist: v0.1.0
+            </a></li>
+          </ul>
+        <h2>Rust</h2>
+          <p>
+            Articles about various topics in Rust
+          </p>
+          <ul>
+            <li><a href="/posts/blog-about-rust.html">
+              Why you should be blogging about Rust
+            </a></li>
+            <li><a href="/posts/1-year-of-rust.html">
+              One year of Rust
+            </a></li>
+            <li><a href="/posts/understanding-where-clauses.html">
+              Understanding where clauses and trait constraints
+            </a></li>
+            <li><a href="/posts/russian-dolls.html">
+              Russian Dolls and clean Rust code
+            </a></li>
+            <li><a href="/posts/pipers.html">
+              Announcing Pipers
+            </a></li>
+          </ul>
+        <h2>Schemers</h2>
+          <p>
+            A series of articles aimed at new Rust users to learn the language
+            by writing a scheme interpreter. These are listed in order.
+          </p>
+          <ul>
+            <li><a href="/posts/scheme-input.html">
+              Input
+            </a></li>
+            <li><a href="/posts/scheme-ex1.html">
+              Exercise 1
+            </a></li>
+            <li><a href="/posts/scheme-parser.html">
+              Parser 1
+            </a></li>
+          </ul>
+        <h2>Haskell and Rust</h2>
+          <p>
+            Articles doing FFI with Haskell and Rust
+          </p>
+          <ul>
+            <li><a href="/posts/haskell-rust.html">
+              FFI with Haskell and Rust
+            </a></li>
+            <li><a href="/posts/rust-haskell.html">
+              Using Haskell in Rust
+            </a></li>
+          </ul>
+        <h2>How do I X in Rust?</h2>
+          <p>
+            A collection of articles aimed at new Rust users to avoid growing pains
+            normally associated with learning the language.
+          </p>
+          <ul>
+            <li><a href="/posts/how-do-i-str-string.html">
+              How do I convert a &amp;str to String in Rust?
+            </a></li>
+            <li><a href="/posts/how-do-i-std-macros.html">
+              How do I use the Standard Library macros in Rust?
+            </a></li>
+          </ul>
+        <h2>JS</h2>
+          <p>
+            Articles regarding my usage of JavaScript.
+          </p>
+          <ul>
+            <li><a href="/posts/schrodingers-bug.html">
+              Schrodinger's Bug: Adventures in Asynchronous Debugging
+            </a></li>
+          </ul>
+      </div>
     );
   }
 }
--- src/client/app/main.jsx
+++ src/client/app/main.jsx
@@ -2,7 +2,40 @@ import React from 'react';
 import { Link } from 'react-router';
 
 class Main extends React.Component {
+  containerData() {
+    if (this.props.location.pathname === '/') {
+      return(
+        <div>
+          <p>
+            Thoughts and musings from a functional programmer who drinks too much coffee.
+          </p>
+          <p>
+            Welcome! This site contains a variety of things including how to get in
+            contact with me, my resume, who I am, as well as a articles I have written
+            regarding technical topics usually related to Rust or Haskell.
+          </p>
+          <p>
+            If you're a recruiter I'm currently not looking for a job right now, but
+            I encourage you to get in contact with me if I ever do decide I'm
+            looking for a new job.
+          </p>
 
+          <div>
+            Recent Articles
+            <ul>
+              <li><a href="/posts/pipers.html">Announcing Pipers</a></li>
+              <li><a href="/posts/scheme-parser.html">Schemers - Parser 1</a></li>
+              <li><a href="/posts/russian-dolls.html">Russian Dolls and clean Rust code</a></li>
+              <li><a href="/posts/scheme-ex1.html">Schemers - Exercise 1</a></li>
+              <li><a href="/posts/scheme-input.html">Schemers - Input</a></li>
+            </ul>
+          </div>
+        </div>
+      );
+    } else {
+      return(this.props.children);
+    }
+  }
   render () {
     return(
     <div>
@@ -24,7 +57,7 @@ class Main extends React.Component {
           </nav>
       </div>
       <div className="container">
-        {this.props.children}
+        {this.containerData()}
       </div>
     </div>
     );
